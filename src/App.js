d50242cc17d78a8786538a0ba1c21ab7ff505418
Ryan Barley <rmbarley@gmail.com>
2021-02-17 23:05:03 -0500

    Fix file extension issue for deploy to vercel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { fetchData } from "./api";
-import { Cards, Chart, CountryPicker } from "./components/";
+import { Cards, Chart, CountryPicker } from "./components/index.js";
 import styles from "./App.module.css";
 
 class App extends React.Component {
